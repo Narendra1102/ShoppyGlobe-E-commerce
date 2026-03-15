ae11a869b99d6d30bd14fc7e45f804eabf5d149d
Narendra1102 <narendra.v02111@gmail.com>
2026-03-15 12:35:56 +0530

    Implemented routing and lazy loading

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import React from 'react'
+import { lazy, Suspense } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+const Home = React.lazy(()=>import("./Components/Home"))
+const Cart = React.lazy(()=>import("./Components/Cart"))
+const ProductDetail = React.lazy(()=>import("./Components/ProductDetail"))
+const Checkout = React.lazy(()=>import("./Components/Checkout"))
+const NotFound = React.lazy(()=>import("./Components/NotFound"))
+
+//React routing configuration
+const router = createBrowserRouter([
+  { 
+    path:"/",
+    element:<App/>,
+    errorElement:<NotFound/>,
+    children:[
+      { path:"/", element:<Home/> },
+      { path:"/product/:id", element:<ProductDetail/> },
+      { path:"/cart", element:<Cart/> },
+      { path:"/checkout", element:<Checkout/> },
+    ]
+
+  },
+  
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Suspense fallback={<h2>Loading...</h2>}>
+    <RouterProvider router={router}/>
+  </Suspense>
 )
