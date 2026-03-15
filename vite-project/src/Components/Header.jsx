e8e0aca0884383b513044a79c9c18f08da1da074
Narendra1102 <narendra.v02111@gmail.com>
2026-03-15 13:00:18 +0530

    Added search bar,cart and checkout

---
+++ vite-project/src/Components/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
+import useFetch from "../utils/useFetch";
+
+function Header() {
+  const products=useFetch()
+  const [filteredProducts,setFilteredProducts]=useState([])
+  function handleSearchText(searchedText){
+    setFilteredProducts(products.filter((product)=>product.title.toLowerCase().includes(searchedText.toLowerCase())))
+  }
+
+  return (
+    <header className="header">
+      <h2 className="logo">ShoppyGlobe</h2>
+
+      <input
+        onChange={(e)=>handleSearchText(e.target.value)} className="search"
+        type="text"
+        placeholder="Search products..."
+      />
+
+      <nav className="navbar">
+        <Link to="/" style={{ textDecoration: "none",color:"white"}}>Home</Link>
+        
+        <Link to="/checkout" style={{ textDecoration: "none",color:"white",marginLeft:"40px"}}>Checkout</Link>
+        <Link to="/cart" style={{ textDecoration: "none",color:"white",marginLeft:"40px"}}>
+           <FaShoppingCart/>
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
