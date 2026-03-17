c71168abd3fa04885973a0c8b8c5e2b4712ce2f3
Narendra1102 <narendra.v02111@gmail.com>
2026-03-17 17:36:53 +0530

    Removed the map lines

--- vite-project/src/Components/ProductItem.jsx
+++ vite-project/src/Components/ProductItem.jsx
@@ -1,29 +1,22 @@
 import { Link } from "react-router-dom";
 
-function ProductItem({products}){
+function ProductItem({product}){
     
     return (
-        products.map((product)=>{
-            
-            return (
-                <div className="product-card" key={product.id}>
-                    <img src={product.images[0]} alt="img" loading="lazy" width="200px" height="200px"/>
-                    <div>
-                        <h2 className="product-title">{product.title}</h2>
-                        <b>${product.price}</b>
-                        <div className="btns">
-                            <Link to={`/product/${product.id}`}>
-                                <button className="det-btn">Details</button>
-                            </Link>
-
-                            
-                        </div>
-                    </div>
-                    
+        <Link to={`/product/${product.id}`} style={{ textDecoration: "none"}}>
+            <div className="product-card">
+                <img src={product.images[0]} alt="img" loading="lazy" width="200px" height="200px"/>
+                <div className="product-det">
+                    <h2 className="product-title">{product.title}</h2>
+                    <h4>${product.price}</h4>
                 </div>
-            )
-        })
+                
+            </div>
+        </Link>
+        
     )
+        
+    
 }
 
 export default ProductItem;
