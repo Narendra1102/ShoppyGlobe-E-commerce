c8de25449e055f39a0c3ec7b6b47050fb28a3f69
Narendra1102 <narendra.v02111@gmail.com>
2026-03-17 17:29:59 +0530

    Filtered the products based on the searched text

--- vite-project/src/Components/ProductList.jsx
+++ vite-project/src/Components/ProductList.jsx
@@ -1,13 +1,29 @@
 import ProductItem from "./ProductItem";
 import useFetch from "../utils/useFetch";
+import { useSelector } from "react-redux";
 
 function ProductList(){
     
     const products=useFetch()
 
+    const searchText=useSelector(state=>state.search.text)
+
+    const filteredProducts=products.filter((product)=>{
+        return product.title.toLowerCase().includes(searchText.toLowerCase())
+    })
+
     return (
         <div className="product-list">
-            <ProductItem products={products}/>
+            {filteredProducts.length > 0 ? (
+                filteredProducts.map(product => (
+                  <ProductItem key={product.id} product={product} />
+                ))
+            ) :
+            (
+                <h2>No products found</h2>
+            )
+            }
+
         </div>
     )
 }
